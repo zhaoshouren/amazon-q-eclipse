13ecfdd9b64610958e82c5d18c4194f4d7be5679
Felix Ding <dingfeli@amazon.com>
2024-09-11 20:07:01 -0700

    Adds basic typehead

---
+++ plugin/src/software/aws/toolkits/eclipse/amazonq/util/CaretMovementReason.java
@@ -0,0 +1,10 @@
+// Copyright 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+package software.aws.toolkits.eclipse.amazonq.util;
+
+public enum CaretMovementReason {
+    UNEXAMINED,
+    MOVEMENT_KEY,
+    TEXT_INPUT
+}
+
