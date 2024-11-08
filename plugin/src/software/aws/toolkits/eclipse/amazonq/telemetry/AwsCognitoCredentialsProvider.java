7647ae76e1ebb566954d1a8dcdfd69bc7fbdc026
Jonathan Breedlove <breedloj@amazon.com>
2024-11-07 17:01:16 -0800

    Add caching to Cognito credentials provider (#161)

--- plugin/src/software/aws/toolkits/eclipse/amazonq/telemetry/AwsCognitoCredentialsProvider.java
+++ plugin/src/software/aws/toolkits/eclipse/amazonq/telemetry/AwsCognitoCredentialsProvider.java
@@ -3,37 +3,72 @@
 
 package software.aws.toolkits.eclipse.amazonq.telemetry;
 
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
 import software.amazon.awssdk.services.cognitoidentity.CognitoIdentityClient;
+import software.amazon.awssdk.services.cognitoidentity.model.Credentials;
 import software.amazon.awssdk.services.cognitoidentity.model.GetCredentialsForIdentityRequest;
-import software.amazon.awssdk.services.cognitoidentity.model.GetCredentialsForIdentityResponse;
 import software.amazon.awssdk.services.cognitoidentity.model.GetIdRequest;
-import software.amazon.awssdk.services.cognitoidentity.model.GetIdResponse;
+import software.amazon.awssdk.utils.SdkAutoCloseable;
+import software.amazon.awssdk.utils.cache.CachedSupplier;
+import software.amazon.awssdk.utils.cache.NonBlocking;
+import software.amazon.awssdk.utils.cache.RefreshResult;
 
-public class AwsCognitoCredentialsProvider implements AwsCredentialsProvider {
+import java.time.temporal.ChronoUnit;
+
+public class AwsCognitoCredentialsProvider implements AwsCredentialsProvider, SdkAutoCloseable {
     private final String identityPoolId;
-    private final CognitoIdentityClient cognitoIdentityClient;
+    private final CognitoIdentityClient cognitoClient;
+    private final CachedSupplier<AwsSessionCredentials> cacheSupplier;
 
-    public AwsCognitoCredentialsProvider(final String identityPoolId, final CognitoIdentityClient cognitoIdentityClient) {
+    public AwsCognitoCredentialsProvider(final String identityPoolId, final CognitoIdentityClient cognitoClient) {
         this.identityPoolId = identityPoolId;
-        this.cognitoIdentityClient = cognitoIdentityClient;
+        this.cognitoClient = cognitoClient;
+        this.cacheSupplier = CachedSupplier.builder(this::updateCognitoCredentials)
+                .prefetchStrategy(new NonBlocking("Cognito Identity Credential Refresh"))
+                .build();
     }
 
     @Override
-    public final AwsSessionCredentials resolveCredentials() {
-        GetIdResponse getIdResponse = cognitoIdentityClient.getId(GetIdRequest.builder()
-                .identityPoolId(identityPoolId)
-                .build());
+    public final AwsCredentials resolveCredentials() {
+        return cacheSupplier.get();
+    }
 
-        GetCredentialsForIdentityResponse getCredentialsResponse = cognitoIdentityClient.getCredentialsForIdentity(GetCredentialsForIdentityRequest.builder()
-                .identityId(getIdResponse.identityId())
-                .build());
+    private RefreshResult<AwsSessionCredentials> updateCognitoCredentials() {
+        String identityId = getIdentityId();
+        Credentials cognitoCredentials = getCredentialsForIdentity(identityId);
 
-        return AwsSessionCredentials.create(
-                getCredentialsResponse.credentials().accessKeyId(),
-                getCredentialsResponse.credentials().secretKey(),
-                getCredentialsResponse.credentials().sessionToken()
+        AwsSessionCredentials sessionCredentials = AwsSessionCredentials.create(
+                cognitoCredentials.accessKeyId(),
+                cognitoCredentials.secretKey(),
+                cognitoCredentials.sessionToken()
         );
+
+        return RefreshResult.builder(sessionCredentials)
+                .staleTime(cognitoCredentials.expiration().minus(1, ChronoUnit.MINUTES))
+                .prefetchTime(cognitoCredentials.expiration().minus(5, ChronoUnit.MINUTES))
+                .build();
+    }
+
+    private String getIdentityId() {
+        GetIdRequest request = GetIdRequest.builder()
+                .identityPoolId(identityPoolId)
+                .build();
+        return cognitoClient.getId(request).identityId();
+    }
+
+    private Credentials getCredentialsForIdentity(final String identityId) {
+        GetCredentialsForIdentityRequest request = GetCredentialsForIdentityRequest.builder()
+                .identityId(identityId)
+                .build();
+        return cognitoClient.getCredentialsForIdentity(request).credentials();
+    }
+
+    @Override
+    public final void close() {
+        cognitoClient.close();
+        cacheSupplier.close();
     }
 }
+
