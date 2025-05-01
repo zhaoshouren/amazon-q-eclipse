dc66942710888b33dec0a5ebf9ac3d6ff181b823
Nicolas Borges <nickdb@amazon.com>
2025-05-01 15:36:51 -0400

    use generic tab params for stop chat response

--- plugin/src/software/aws/toolkits/eclipse/amazonq/chat/models/StopChatResponseParams.java
+++
@@ -1,8 +0,0 @@
-package software.aws.toolkits.eclipse.amazonq.chat.models;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record StopChatResponseParams(
-    @JsonProperty("tabId")
-        String tabId) {
-}
