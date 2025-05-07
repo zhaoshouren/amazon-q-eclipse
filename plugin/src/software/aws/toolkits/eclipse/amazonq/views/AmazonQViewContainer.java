0f9e4601ace3b5be394f7bd7de9215d99d2d55fb
Jonathan Breedlove <breedloj@amazon.com>
2025-05-06 17:41:21 -0700

    Add Checkstyle rule for copyright header

--- plugin/src/software/aws/toolkits/eclipse/amazonq/views/AmazonQViewContainer.java
+++ plugin/src/software/aws/toolkits/eclipse/amazonq/views/AmazonQViewContainer.java
@@ -1,4 +1,4 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Copyright 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
 package software.aws.toolkits.eclipse.amazonq.views;
--- plugin/src/software/aws/toolkits/eclipse/amazonq/views/model/ChatCodeReference.java
+++ plugin/src/software/aws/toolkits/eclipse/amazonq/views/model/ChatCodeReference.java
@@ -1,5 +1,5 @@
-//Copyright 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-//SPDX-License-Identifier: Apache-2.0
+// Copyright 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 
 package software.aws.toolkits.eclipse.amazonq.views.model;
 
