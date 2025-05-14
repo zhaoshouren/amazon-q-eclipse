d46177f2dcf8cbb5c709dd501cddce7582915653
Jonathan Breedlove <breedloj@amazon.com>
2025-05-14 11:44:06 -0700

    Use isExternal flag in showDocument LSP callbacks

--- plugin/src/software/aws/toolkits/eclipse/amazonq/lsp/AmazonQLspClient.java
+++ plugin/src/software/aws/toolkits/eclipse/amazonq/lsp/AmazonQLspClient.java
@@ -57,8 +57,5 @@ public interface AmazonQLspClient extends LanguageClient {
 
     @JsonNotification("aws/didCreateDirectory")
     void didCreateDirectory(Object params);
-<<<<<<< HEAD
 
-=======
->>>>>>> 49b54e6 (Implement refresh LSP callbacks)
 }
