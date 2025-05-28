47251737c833d1d52c3f2c39f3a94b79f19967b2
Ishan Taldekar <taldekar@amazon.com>
2025-05-28 17:46:48 -0400

    Fix missing lsp notification when default profile set

--- plugin/src/software/aws/toolkits/eclipse/amazonq/views/CustomizationDialog.java
+++ plugin/src/software/aws/toolkits/eclipse/amazonq/views/CustomizationDialog.java
@@ -315,6 +315,7 @@ public final class CustomizationDialog extends Dialog {
     protected void okPressed() {
         if (this.responseSelection.equals(ResponseSelection.AMAZON_Q_FOUNDATION_DEFAULT)) {
             Activator.getPluginStore().remove(Constants.CUSTOMIZATION_STORAGE_INTERNAL_KEY);
+            ThreadingUtils.executeAsyncTask(() -> CustomizationUtil.triggerChangeConfigurationNotification());
             Display.getCurrent()
                     .asyncExec(() -> CustomizationUtil.showNotification(Constants.DEFAULT_Q_FOUNDATION_DISPLAY_NAME));
         } else if (Objects.nonNull(this.getSelectedCustomization())
