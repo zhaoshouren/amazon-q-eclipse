0e7e5bd631e9a44b47ae4f9a481a42dbc08d2b76
Shruti Sinha <44882001+shruti0085@users.noreply.github.com>
2025-08-27 09:12:00 -0700

    feat: Add ability to override manifest urls (#509)
    Adds ability to override manifest urls when local testing with the env variable Q_MANIFEST. When set, this url will be used to fetch and launch the language server from.

--- plugin/src/software/aws/toolkits/eclipse/amazonq/providers/lsp/LspManagerProvider.java
+++ plugin/src/software/aws/toolkits/eclipse/amazonq/providers/lsp/LspManagerProvider.java
@@ -3,9 +3,12 @@
 
 package software.aws.toolkits.eclipse.amazonq.providers.lsp;
 
+import org.apache.commons.lang3.StringUtils;
+
 import software.aws.toolkits.eclipse.amazonq.lsp.manager.DefaultLspManager;
 import software.aws.toolkits.eclipse.amazonq.lsp.manager.LspConstants;
 import software.aws.toolkits.eclipse.amazonq.lsp.manager.LspManager;
+import software.aws.toolkits.eclipse.amazonq.plugin.Activator;
 
 public final class LspManagerProvider {
 
@@ -29,8 +32,20 @@ public final class LspManagerProvider {
     private static LspManager createLspManager() {
         return DefaultLspManager.builder()
             .withLspExecutablePrefix(LspConstants.CW_LSP_FILENAME)
-            .withManifestUrl(LspConstants.CW_MANIFEST_URL)
+            .withManifestUrl(getManifestUrl())
             .build();
     }
 
+    public static String getManifestUrl() {
+        var manifestUrl = getEnvironmentVariable("Q_MANIFEST");
+        if (StringUtils.isBlank(manifestUrl) || !manifestUrl.endsWith("manifest.json")) {
+            return LspConstants.CW_MANIFEST_URL;
+        }
+        Activator.getLogger().info("Using manifest url override: " + manifestUrl);
+        return manifestUrl;
+    }
+
+    public static String getEnvironmentVariable(final String variableName) {
+        return System.getenv(variableName);
+    }
 }
